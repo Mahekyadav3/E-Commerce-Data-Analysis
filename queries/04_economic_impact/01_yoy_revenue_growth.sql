74d170270f3f876b59170b541f12a32338196c72
Mahekyadav3 <yadavmahek00@gmail.com>
2026-06-03 22:35:26 +0530

    Create 01_yoy_revenue_growth.sql

---
+++ queries/04_economic_impact/01_yoy_revenue_growth.sql
@@ -0,0 +1,36 @@
+-- ============================================================
+-- Query  : Top 5 States by Average Freight Value
+-- Section: 05 - Delivery & Freight Analysis
+-- Author : Mahek
+-- Tool   : Google BigQuery
+-- ============================================================
+WITH monthly_data AS (
+SELECT
+EXTRACT(YEAR FROM o.order_purchase_timestamp) AS year,
+EXTRACT(MONTH FROM o.order_purchase_timestamp) AS month,
+p.payment_value
+FROM
+businesscase_1.payments p
+JOIN
+businesscase_1.orders o ON p.order_id = o.order_id
+WHERE
+EXTRACT(YEAR FROM o.order_purchase_timestamp) IN (2017, 2018)
+AND EXTRACT(MONTH FROM o.order_purchase_timestamp) <= 8
+),
+yearly_total AS (
+SELECT
+year,
+SUM(payment_value) AS total_payment
+FROM
+monthly_data
+GROUP BY
+year
+)
+SELECT
+ROUND(
+(MAX(CASE WHEN year = 2018 THEN total_payment END) -
+MAX(CASE WHEN year = 2017 THEN total_payment END)) * 100 /
+MAX(CASE WHEN year = 2017 THEN total_payment END),
+2
+) AS percent_increase
+FROM yearly_total;
