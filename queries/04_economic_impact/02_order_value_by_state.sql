c68e0881aa4709ec4c92185aaee61c9082c4488b
Mahekyadav3 <yadavmahek00@gmail.com>
2026-06-03 22:53:38 +0530

    Update 02_order_value_by_state.sql

--- queries/04_economic_impact/02_order_value_by_state.sql
+++ queries/04_economic_impact/02_order_value_by_state.sql
@@ -1,6 +1,6 @@
 -- ============================================================
 -- Query  : Top 5 States by Average Freight Value
--- Section: 05 - Delivery & Freight Analysis
+-- Section: 04 - Economic Impact
 -- Author : Mahek
 -- Tool   : Google BigQuery
 -- ============================================================
