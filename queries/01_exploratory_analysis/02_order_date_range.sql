1e798c8871b04f83ca39e46a9d194b3596622042
Mahekyadav3 <yadavmahek00@gmail.com>
2026-06-03 22:18:59 +0530

    Update 02_order_date_range.sql

--- queries/01_exploratory_analysis/02_order_date_range.sql
+++ queries/01_exploratory_analysis/02_order_date_range.sql
@@ -1,3 +1,9 @@
+-- ============================================================
+-- Query  : Customer Table Schema
+-- Section: 01 - Exploratory Analysis
+-- Author : Mahek
+-- Tool   : Google BigQuery
+-- ============================================================
 select
 max(order_purchase_timestamp) as last_order_date,
 min(order_purchase_timestamp) as first_order_date
