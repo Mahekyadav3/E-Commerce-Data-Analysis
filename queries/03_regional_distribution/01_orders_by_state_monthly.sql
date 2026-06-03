f4688f488bdd32374ae32248ec0a955bdb3ae2b5
Mahekyadav3 <yadavmahek00@gmail.com>
2026-06-03 22:29:17 +0530

    Create 01_orders_by_state_monthly.sql

---
+++ queries/03_regional_distribution/01_orders_by_state_monthly.sql
@@ -0,0 +1,7 @@
+select distinct c.customer_state,
+extract(month from o.order_purchase_timestamp ) as month,
+count(*) as no_of_orders
+from `businesscase_1.orders` o join `businesscase_1.customers` c 
+on o.customer_id = c.customer_id
+group by c.customer_state, extract(month from o.order_purchase_timestamp )
+order by c.customer_state,month;
