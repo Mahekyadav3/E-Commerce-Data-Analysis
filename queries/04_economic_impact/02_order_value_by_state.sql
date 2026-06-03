6666e9c73809aeaeab9d821def7640d820513482
Mahekyadav3 <yadavmahek00@gmail.com>
2026-06-03 22:38:21 +0530

    Create 02_order_value_by_state.sql

---
+++ queries/04_economic_impact/02_order_value_by_state.sql
@@ -0,0 +1,13 @@
+-- ============================================================
+-- Query  : Top 5 States by Average Freight Value
+-- Section: 05 - Delivery & Freight Analysis
+-- Author : Mahek
+-- Tool   : Google BigQuery
+-- ============================================================
+select distinct c.customer_state,
+round(sum(p.payment_value),2) as total_value_of_orderprice,
+round(avg(p.payment_value),2) as average_of_orderprice
+from `businesscase_1.payments` p join `businesscase_1.orders` o on p.order_id = o.order_id
+join `businesscase_1.customers` c on c.customer_id = o.customer_id
+group by c.customer_state
+order by c.customer_state;
